67ec2bdbcdb950bf43c151fd6f89c26f0bcf9e27
lynellsinger <lynell.r.singer@gmail.com>
2021-01-10 12:00:28 -0800

    Updated searchBar and added redux-form from npm

--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
 
 class SearchBar extends Component {
+    renderInput(Field) {
+        return <input type="text" placeholder="Search DailySmarty" {...field.input} />
+    }
+    
     render() {
+        const { handleSubmit } = this.props;
         return (
             <form>
-                <input placeholder="Search DailySmarty"/>
+                <Field name="query" component={this.renderInput} />
             </form>
         )
     }
 }
+
+searchBar = reduxForm({
+    form: 'SearchBar'
+})(SearchBar);
 
 export default SearchBar;
