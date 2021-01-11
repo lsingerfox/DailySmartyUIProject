48e8ef556bb05af601adf3e810e70bb9b3d4ceb6
lynellsinger <lynell.r.singer@gmail.com>
2021-01-10 21:53:51 -0800

    Added redux-thunk to package & package-lock, updated connection for search bar and recent posts

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -3,11 +3,12 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+import thunk from 'redux-thunk';
 import Home from "./components/home";
 import Results from "./components/results";
 import reducers from "./reducers";
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
 
 import "./style/main.scss";
 
