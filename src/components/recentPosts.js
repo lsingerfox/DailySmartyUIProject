d72ff4b3f5ee02972f41e06a096be8e6a673a4ad
lynellsinger <lynell.r.singer@gmail.com>
2021-01-11 21:12:18 -0800

    Cleaned up code for posts to make recentPosts.js more dynamic and made new posts.js file

--- src/components/recentPosts.js
+++ src/components/recentPosts.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+
 import * as actions from '../actions';
+import Posts from './posts';
 
 class RecentPosts extends Component {
     componentDidMount() {
@@ -11,9 +13,7 @@ class RecentPosts extends Component {
         const posts = this.props.recentPosts.map((post, index) => {
             if (index < 3) {
                 return (
-                    <li key={index}>
-                        {post.title}
-                    </li>
+                    <Posts {...post} key={index} />
                 )
             }
         })
