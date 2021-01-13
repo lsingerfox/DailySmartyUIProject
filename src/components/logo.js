b64c5d97857489fbf70324727f73f918a2110966
lynellsinger <lynell.r.singer@gmail.com>
2021-01-12 20:36:13 -0800

    Added new results style file, removed styles from logo.scss, updated import for main.scss, added in-line styles for logo and added manual logo style in results and search bar in results

--- src/components/logo.js
+++ src/components/logo.js
@@ -2,9 +2,13 @@ import React, { Component } from 'react';
 
 class Logo extends Component {
     render() {
+        const size = {
+            height: this.props.size ? this.props.size : 100,
+            width: this.props.size ? this.props.size : 100
+        }
         return (
             <div className="logo-main">
-                <img alt="Daily Smarty Logo" src="/assets/ds_circle_logo.png"/>
+                <img style={size} alt="Daily Smarty Logo" src="/assets/ds_circle_logo.png"/>
             </div>
         )
     }
