3eda9e5a8fbe2d9c6b5117715792b0ac4631d90d
lynellsinger <lynell.r.singer@gmail.com>
2021-01-21 18:22:03 -0800

    Installed @fortawesome/react-fontawesome, fixed searchBar icon error, and styled recent posts

--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
 import { withRouter } from 'react-router-dom';
 
+
 class SearchBar extends Component {
     handleFormSubmit = function({query}) {
         this.props.onSubmit(query);
     }
 
     renderInput(field) {
-        return <input type="text" placeholder="Search DailySmarty" {...field.input} />
+        return <input type="text" placeholder="&#xf002; Search DailySmarty" {...field.input} />
     }
     
     render() {
